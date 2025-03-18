858e622516b5c05cc1c56f5b3b8012a0b9b2dd2a
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-18 22:05:59 +0000

    Update on 2025-03-18 22:05:59

--- TheRequirements.h
+++ TheRequirements.h
@@ -4,18 +4,20 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
  //Sad wee can't do WTFPL
-#define Layer0Setup(description,layer) \
-    MODULE_DESCRIPTION(description); \
+ //https://github.com/we-make-software/TheRequirements?tab=License-1-ov-file
+ //Damm!! you GPL!!
+#define Layer0Setup(description,version,build) \
+    MODULE_DESCRIPTION(description "\version: " #version "\build:" #build); \
     MODULE_LICENSE("GPL"); \
     MODULE_AUTHOR("We-Make-Software.Com"); \
     static int __init SetupInitProject(void) { \
         Layer0Start(); \
-        printk(KERN_INFO "%s: Start\nTimestamp: %llu\n", description, ktime_get_real_seconds()); \
+        printk(KERN_INFO "%s: Start\nVersion: %s\nBuild: %s\nTimestamp: %llu\n", description, #version, #build, ktime_get_real_seconds()); \
         return 0; \
     } \
     static void __exit SetupExitProject(void) { \
         Layer0End(); \
-        printk(KERN_INFO "%s: End\nTimestamp: %llu\n", description, ktime_get_real_seconds()); \
+        printk(KERN_INFO "%s: End\nVersion: %s\nBuild: %s\nTimestamp: %llu\n", description, #version, #build, ktime_get_real_seconds()); \
     } \
     module_init(SetupInitProject); \
     module_exit(SetupExitProject);
