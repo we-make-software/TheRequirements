3d8e1eb80a678d73aa7c240e590c80ed44f973eb
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-20 19:32:55 +0000

    Update on 2025-03-20 19:32:55

--- TheRequirements.h
+++ TheRequirements.h
@@ -3,7 +3,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/slab.h> 
+#include <linux/slab.h>
 //Sad wee can't do WTFPL
 //https://github.com/we-make-software/TheRequirements?tab=License-1-ov-file
 //Damm!! you GPL!!Show me!!
@@ -13,12 +13,10 @@
     MODULE_AUTHOR("We-Make-Software.Com"); \
     static int __init SetupInitProject(void) { \
         Layer0Start(); \
-        printk(KERN_INFO "%s: Start\nVersion: %s\nBuild: %s\nTimestamp: %llu\n", description, #version, #build, ktime_get_real_seconds()); \
         return 0; \
     } \
     static void __exit SetupExitProject(void) { \
         Layer0End(); \
-        printk(KERN_INFO "%s: End\nVersion: %s\nBuild: %s\nTimestamp: %llu\n", description, #version, #build, ktime_get_real_seconds()); \
     } \
     module_init(SetupInitProject); \
     module_exit(SetupExitProject);
