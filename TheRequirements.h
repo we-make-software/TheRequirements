68d43ca0d2cc3f81a12383042ddcfd97667e04c0
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-21 17:04:04 +0000

    Update on 2025-03-21 17:04:04

--- TheRequirements.h
+++ TheRequirements.h
@@ -7,16 +7,16 @@
 //Sad wee can't do WTFPL
 //https://github.com/we-make-software/TheRequirements?tab=License-1-ov-file
 //Damm!! you GPL!!Show me!!
-#define Layer0Setup(description,version,build) \
+#define Setup(description,version,build) \
     MODULE_DESCRIPTION(description "\version: " #version "\build:" #build); \
     MODULE_LICENSE("GPL"); \
     MODULE_AUTHOR("We-Make-Software.Com"); \
     static int __init SetupInitProject(void) { \
-        Layer0Start(); \
+        Start(); \
         return 0; \
     } \
     static void __exit SetupExitProject(void) { \
-        Layer0End(); \
+        End(); \
     } \
     module_init(SetupInitProject); \
     module_exit(SetupExitProject);
